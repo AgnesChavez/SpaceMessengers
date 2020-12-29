b861f43d707eaaf09b755803e0adf0329fd589ed
Roy Macdonald <macdonald.roy@gmail.com>
2020-12-29 02:38:25 -0300

    A lot of improvements

---
+++ functions/src/utils.js
@@ -0,0 +1,14 @@
+
+
+
+function getRandomFloat(min, max) {
+  return (Math.random() * (max - min + 1) + min);
+}
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+exports.getRandomInt = getRandomInt;
+exports.getRandomFloat = getRandomFloat;
