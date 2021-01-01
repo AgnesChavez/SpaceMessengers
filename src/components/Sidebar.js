b7d39ec9879d5baaf82305b637f88cfbea6c37fe
Roy Macdonald <macdonald.roy@gmail.com>
2020-12-31 23:00:32 -0800

    Sidebar parsing and structured correctly. needs button and collapsible implementation

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,80 +1,54 @@
-import React, { Component, useRef, useState } from "react";
+import React from "react";
 
 import { auth } from "../services/firebase";
 import { db } from "../services/firebase";
-import firebase from 'firebase/app';
+
 import 'firebase/firestore';
 
 // import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
 
-import { Collapsible, CollapsibleItem } from 'react-materialize';
-import { getUserFromDb, getTeamForUserWorkshop } from "../helpers/userManagement";
-import { getQueryData } from "../helpers/db";
 
-import { Link } from 'react-router-dom';
-
-import { Collection, CollectionItem, Button, Row, Col, SideNav, SideNavItem } from 'react-materialize';
+import { getUserFromDb } from "../helpers/userManagement";
 
 import { userTypes } from "../helpers/Types"
 
 
 function SidebarUser(props)
 {
-  let usr = getUserFromDb(props.uid);
-  if(usr){
-    return (<>
-      <CollectionItem className="avatar">
-        <img
-          alt=""
-          className="circle"
-          src={usr.photoURL}
-        />
-        <span className="title">
-          {usr.displayName}
-        </span>
-      </CollectionItem>
-      </>
-      );
+  let [usr, usrLoading] = useDocumentData(db.collection('users').doc(props.uid));
+
+  // let usr = getUserFromDb(props.uid);
+  if(usr && ! usrLoading){
+
+    if ('color' in usr){
+      return (<li key={usr.id} style={{color: usr.color}}>{usr.displayName}</li>);  
+    }else{
+      return (<li key={usr.id} >{usr.displayName } </li>);  
+    }
+    
   }
   return null;
 }
-
-function SidebarBoards(props)
+function RenderUsers(props)     
 {
-  let boardData = getQueryData(db.collection("bords").doc(props.boardId));
-  if(!boardData) return null;
-  return (
-     <CollectionItem href="#">
-        {boardData.name}
-      </CollectionItem>
-    )
+    return (<>
+      <div>
+      <p className="sidebarName"> {props.name}</p>
+    <ul style={{"paddingLeft": 20+'px'}}>
+            {props.users.map( i => <SidebarUser key={i} uid={i} />)}
+    </ul>
+  </div>
+    </>
+    );
 }
-
-
 function SidebarTeam(props){
   return (<>
-  <CollapsibleItem
-    expanded={false}
-    header={props.team.data().name}
-    node="div"
-  >
-  <Collapsible accordion>
+  
+  <p>{props.team.name}</p>
 
-   <CollapsibleItem
-    expanded={false}
-    header="Members"
-    node="div"
-  >
-  <Collection>
-    {props.team.data().members.map( i => <SidebarUser uid={i} />)}
-    </Collection>
-  </CollapsibleItem>
-
-    <SidebarBoardsCollection user={props.user} teamId={props.team.id} />
-
-  </Collapsible>
-  </CollapsibleItem>
+    <RenderUsers name="Members" users={props.team.members} ></RenderUsers>
+    <SidebarBoardsCollection user={props.user} teamId={props.team.id} />  
   </>
   );
 }
@@ -83,29 +57,65 @@ function SidebarTeam(props){
 
 function SidebarWorkshop(props){
   return (<>
-  <CollapsibleItem
-    expanded={false}
-    header={props.workshop.data().name}
-    node="div"
-  >
-  <Collapsible accordion>
-
-   <CollapsibleItem
-    expanded={false}
-    header="Instructors"
-    node="div"
-  >
-  <Collection>
-    {props.workshop.data().instructors.map( i => <SidebarUser uid={i} />)}
-    </Collection>
-  </CollapsibleItem>
+    <div style={{"paddingLeft": 20+'px'}}>
+    <p>{props.workshop.name}</p>
+    <RenderUsers name="Instructors" users={props.workshop.instructors} ></RenderUsers>
 
     <SidebarTeamCollection user={props.user} workshopId={props.workshop.id} />
-
-  </Collapsible>
-  </CollapsibleItem>
+    </div>
   </>
   );
+}
+
+
+function SidebarBoardsCollection(props){
+      const [boards, boardsLoading] = useCollectionData(
+        db.collection("boards").where("teamId", "==", props.teamId));
+      // if(!boards) return null;
+    return (
+        <>
+            <p>Boards</p>
+            <ul style={{"paddingLeft": 20+'px'}}>
+                {!boardsLoading && boards && boards.map(board => 
+                    <li key={board.id}>
+                        {board.name}</li>
+                    )}
+            </ul>
+        </>
+    );
+}
+
+function SidebarTeamCollection(props){
+    
+
+    const [teams, teamsLoading] = useCollectionData(getTeamsQueryForUser(props.user, props.workshopId)); 
+    return (
+        <>
+            <div>
+            <p>Teams</p>
+            
+            <ul style={{"paddingLeft": 20+'px'}}>
+                { !teamsLoading && teams && teams.map(team => <SidebarTeam key={team.id} team={team} user={props.user}/> )}
+            </ul>
+            </div>
+        </>
+    );
+}
+
+
+function SidebarWorkshopCollection(props){
+    const [workshops] = useCollectionData(getWorkshopQueryForUser(props.user)); 
+    if(!workshops)return null;
+    return (
+        <>
+            <div>
+              <p>Workshops</p>
+              <ul style={{"paddingLeft": 20+'px'}}>
+                  {workshops && workshops.map(doc => <SidebarWorkshop key={doc.id} workshop={doc} user={props.user} /> )}
+              </ul>
+            </div>
+        </>
+    );
 }
 
 function getTeamsQueryForUser(usr, workshopId){
@@ -116,80 +126,6 @@ let teams = db.collection("teams").where("workshopId", "==", workshopId);
   }
   return teams;
 }
-
-function SidebarBoardsCollection(props){
-      const [boards] = useCollectionData(
-        db.collection("boards").where("teamId", "==", props.teamId));
-        // getTeamsQueryForUser(props.user, )); 
-
-      if(!boards) return null;
-    return (
-        <>
-            <CollapsibleItem
-                    expanded={true}
-                    header="Boards"
-                    node="div"
-                >
-            <Collapsible accordion>
-                
-                    {boards.map(board => 
-                            <SideNavItem
-                             href="#!"
-                            waves
-                            >
-                            {board.data().name}
-                            </SideNavItem>
-                        )}
-
-            </Collapsible>
-            </CollapsibleItem>
-        </>
-    );
-}
-
-function SidebarTeamCollection(props){
-    
-
-    const [teams] = useCollectionData(getTeamsQueryForUser(props.user, props.workshopId)); 
-    if(!teams)return null;
-    return (
-        <>
-            <CollapsibleItem
-                    expanded={true}
-                    header="Teams"
-                    node="div"
-                >
-            <Collapsible accordion>
-                
-                    {teams.map(team => <SidebarTeam team={team} user={props.user}/> )}
-
-            </Collapsible>
-            </CollapsibleItem>
-        </>
-    );
-}
-
-
-
-function SidebarWorkshopCollection(props){
-    const [workshops] = useCollectionData(getWorkshopQueryForUser(props.user)); 
-    if(!workshops)return null;
-    return (
-        <>
-            <Collapsible accordion>
-                <CollapsibleItem
-                    expanded={true}
-                    header="Workshops"
-                    node="div"
-                >
-                    {workshops.map(doc => <SidebarWorkshop workshop={doc} user={props.user} /> )}
-
-                </CollapsibleItem>
-            </Collapsible>
-        </>
-    );
-}
-
 
 
 function getWorkshopQueryForUser(usr)
@@ -212,45 +148,8 @@ export  function Sidebar() {
   if(!usr) return "";
 
 
-
-  
-
 return (<>
- <SideNav
-    className="black SidebarNav"
-    id="SidebarLeft"
-
-    options={{
-      draggable: true
-    }}
-    trigger={<Button node="button">SIDE NAV DEMO</Button>}
-  >
-
-<SideNavItem className="black-text"
-      user={{
-        image:  usr.photoURL ,
-        name: (usr.displayName || usr.name)
-      }}
-      userView
-    />
-  
-
-
   <SidebarWorkshopCollection user={usr} />
-
-</SideNav>
-
-{/* {auth().currentUser */}
-{/*             ? <><div> */}
-{/*             <Link className="profileButton" to="/profile">Profile</Link> */}
-{/*             <div className="loggedInAs"> */}
-{/*               Logged in as: <strong className="text-info">{auth().currentUser.email}</strong> */}
-{/*             </div> */}
-{/*             </div> */}
-{/*               <Button  onClick={() => auth().signOut()}>Logout</Button> */}
-{/*               </> */}
-{/*             : <Button href="/login">Log In</Button>  */}
-{/*             } */}
   </>)
 }
 
