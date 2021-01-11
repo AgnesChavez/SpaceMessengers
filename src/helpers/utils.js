d673af478a09000d27b4f6a3f13005b8c500a6b0
Roy Macdonald <macdonald.roy@gmail.com>
2021-01-11 13:31:18 -0800

    added isFunction function

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,3 +2,8 @@
 export function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+
+export function isFunction(functionToCheck) {
+ return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
