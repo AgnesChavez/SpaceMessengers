db301c1b0e9c14f44fdab3588039fb3e6ffd606e
Roy Macdonald <macdonald.roy@gmail.com>
2021-01-22 05:20:01 -0800

    gallery updates

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,60 +1,93 @@
 
-import React,  { useEffect, useState} from "react";
+import React,  { useEffect, useState, useRef} from "react";
+
 import CenteredPreloader from '../components/CenteredPreloader'
+
 import { useDownloadURL } from 'react-firebase-hooks/storage';
 
 import { auth, storageRef } from "../services/firebase";
 
 import '../css/gallery.css';
 
-function LoadAndShowImage(props){
-	// let ref = firebase.storage().ref('images/' + auth().currentUser.uid + "/cvWHAAzbDEMUdkXS2d8s.png");
-	// console.log(props.img);
-  const [value, loading] = useDownloadURL(props.img);
-  	
-  
-  return (<>
-    <div className="gridImg">
+import { Icon, Button, MediaBox } from 'react-materialize';
 
-      {loading && <CenteredPreloader title={"Loading images"}/>}
-      {!loading && value && <img src={value} alt=""/> }
-    </div>
-    </>
-  );
-};
+import { Link } from 'react-router-dom';
+
+function LoadAndShowImage(props){
+	
+	const [value, loading] = useDownloadURL(props.img);
+
+  	
+  	return (<>
+    	<div id={props.img.name} className="gridImg">
+      		{loading && <CenteredPreloader title={"Loading images"}/>}
+      		{!loading && value && 
+
+			<MediaBox
+  				options={{
+  				  	inDuration: 200,
+  				  	outDuration: 200
+  				}}
+				>
+			  	<img
+			    	alt=""
+			    	src={value}
+			    	className=" materialboxed galleryImg"
+			  	/>
+			</MediaBox>}			
+    	</div>
+    </>);
+}
 
 
 
 export default  function Gallery(props) {
     
-
-let listRef = storageRef.child('images/' + auth().currentUser.uid);
+	let listRef = storageRef.child('images/' + auth().currentUser.uid);
 	
 	let [imagesRefs, setImagesRefs] = useState(null);
 	let [refsLoaded, setRefsLoaded] = useState(false);
-useEffect(()=>{
-	if(!refsLoaded){
-	listRef.listAll().then(res=>{
-		
-			setRefsLoaded(true);
-			setImagesRefs(res.items);
-		 	// console.log(res.items);
-		
-	}).catch((error)=> {
-  		console.log(error);
+	
+	
+
+	useEffect(()=>{
+		if(!refsLoaded){
+			listRef.listAll().then(res=>{
+				setRefsLoaded(true);
+				setImagesRefs(res.items);
+			}).catch((error)=> {
+  				console.log("Loading gallery failed with error:", error);
+			});
+		}
 	});
-	}
-});
 
+
+	return <>
 	
-	
+		<div id="gallery"> 
+  <Link to={"/board"} >
+	<Button
+  	
+    className=""
+    node="button"
+    small
+    tooltip="go back to boards"
+    waves="light"
+    floating
+  	icon={<Icon>arrow_back</Icon>}
+    />
+</Link>
 
 
-
-return <>
-	<div id="gallery"> 
-		{(imagesRefs!==null && imagesRefs.length >0) ? imagesRefs.map(i => <LoadAndShowImage key={i.fullPath} img={i}/>):
-		<h4> Your image gallery is empty! </h4>}
-	</div>
+		<div id="GalleryImgs">
+			{(imagesRefs!==null && imagesRefs.length >0) ?
+				imagesRefs.map(i => <LoadAndShowImage 
+			 							key={i.fullPath}
+			 							img={i}/>)
+				:
+				<h4> Your image gallery is empty! </h4>
+			}
+			</div>
+		</div>
 	</>
 }
