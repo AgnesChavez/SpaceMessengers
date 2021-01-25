5dd5693d49f12fc0ef793b4e83907992679c4d63
Roy Macdonald <macdonald.roy@gmail.com>
2021-01-24 22:03:29 -0800

    Changed secondary title and removed footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,14 +12,13 @@ export default function HomePage(props) {
                 <div className="spaceMessengersBg" >
                     <div className="container">
                         <h1 className="">Welcome to Space Messengers</h1>
-                        <h5 className="">A mixed reality projection and youth workshop</h5>
+                        <h5 className="">A mixed reality installation and youth workshop</h5>
                         <div style={{marginTop: "100px"}}>
                             <Link className="btn waves-effect waves-light btn" to={(auth().currentUser?"/board":"/login")}>Login to Your Account</Link>
                         </div>
                     </div>
                 </div>
             </section>
-            <Footer></Footer>
         </div>
     )
 }
