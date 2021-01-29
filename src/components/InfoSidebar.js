be0e7a5b9de457f5679e7da1fe042675a0be4d82
Roy Macdonald <macdonald.roy@gmail.com>
2021-01-29 15:55:02 -0800

    Info sidebar cleanup

--- src/components/InfoSidebar.js
+++ src/components/InfoSidebar.js
@@ -12,9 +12,6 @@ export function InfoSidebar(props){
     const tabsRef = useRef(null);
     const sidenavRef = useRef(null);
 
-    // const [translate, setTranslate] = useState(0);
-    // let translateMax  = 300;
-
     useEffect(()=>{
         let el = document.getElementById('InfoSidebar');
         if(el){
@@ -23,19 +20,6 @@ export function InfoSidebar(props){
             }
             if(!sidenavRef.current){
                 sidenavRef.current = window.M.Sidenav.init(el, {  draggable: true, edge: "right"  });   
-                // console.log("init InfoSidebar");
-                // 
-                // if(props.isOpen){
-                //     sidenavRef.current.open();
-                // }else{
-                //     sidenavRef.current.close();
-                // }
-                // sidenavRef.current.isOpen = props.isOpen;
-                // console.log("isOpen", props.isOpen);
-                // console.log(el.style);
-                // el.style.transform = "translateX("+ ((props.isOpen)?0:translateMax) +"px)";
-                // console.log(el.style);
-                //style={{transform: "translateX("+ (translateMax) +"px)"}}
             }
         }
         return () => {
@@ -44,58 +28,39 @@ export function InfoSidebar(props){
         };
     });
 
-
-    
-// 
-//     if(props.isOpen && translate !== 0){
-//         setTranslate(0);
-//     }else if(!props.isOpen && translate !== translateMax){
-//         setTranslate(translateMax);
-//     }
-
     if(!props.boardId  ) return "";
-
     
-
-    // "translateX("+ (props.isOpen?300:0) +")";
-    // console.log(translate);
-    
-    // console.log("translate", translate);
-    
-
-
     return (<>
-        <div id="InfoSidebarContainer" style={{transform: "translateX("+(props.isOpen?0:300)+"px)"}}>
+    <div id="InfoSidebarContainer" style={{transform: "translateX("+(props.isOpen?0:300)+"px)"}}>
         <ul id="InfoSidebar" className="sidenav sidenav-fixed black white-text" >
 
-    <div id="TabsContainer" className="fullheight">
-
-    <ul id="InfoSidebarTabs" className="tabs tabs-fixed-width z-depth-1 black white-text">
-        <li className="tab"><a href="#chatTab">Chat</a></li>
-        <li className="tab"><a href="#commentsTab">Comments</a></li>
-    </ul>
-    <div id="chatTab" className="col s12">
-        <Chat collection="chats"
-            group={props.boardId} 
-            containerClass="sidebar-chat"
-            isComment={false}
-            getUser={props.getUser}
-        />
-    </div>
-        <div id="commentsTab" className="col s12">
-            {props.selected?
-                <Chat collection="comments"
-                    group={props.selected.id}
-                    containerClass="sidebar-chat"
-                    isComment={true}
-                    bgColor={props.selected.color}
-                    getUser={props.getUser}
-                />
-            :<h6>Select a message on the board and you will see its comments here</h6>
-            }      
-        </div>
-    </div>
-    </ul>
+            <div id="TabsContainer" className="fullheight">
+                <ul id="InfoSidebarTabs" className="tabs tabs-fixed-width z-depth-1 black white-text">
+                    <li className="tab"><a href="#chatTab">Chat</a></li>
+                    <li className="tab"><a href="#commentsTab">Comments</a></li>
+                </ul>
+                <div id="chatTab" className="col s12">
+                    <Chat collection="chats"
+                        group={props.boardId} 
+                        containerClass="sidebar-chat"
+                        isComment={false}
+                        getUser={props.getUser}
+                    />
+                </div>
+                <div id="commentsTab" className="col s12">
+                    {props.selected?
+                        <Chat collection="comments"
+                            group={props.selected.id}
+                            containerClass="sidebar-chat"
+                            isComment={true}
+                            bgColor={props.selected.color}
+                            getUser={props.getUser}
+                        />
+                    :<h6>Select a message on the board and you will see its comments here</h6>
+                    }      
+                </div>
+            </div>
+        </ul>
     </div>
     </>);
 }
