34a81d66c9d9f6362f509690dd9e4fd2d09ce3ab
Roy Macdonald <macdonald.roy@gmail.com>
2021-02-16 23:20:07 -0800

    Gallery using thumbs and new layout

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -16,9 +16,9 @@ import { Link } from 'react-router-dom';
 
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 
-import { userTypes } from "../helpers/Types"
+import { userTypes, ImageData } from "../helpers/Types"
 
-
+import { addDataToDb } from '../helpers/db';
 
 function getWidth() {
   	return Math.max(
@@ -48,11 +48,11 @@ function deleteImg(img){
 
 
 function LoadAndShowImage(props){
-	const [downloadUrl, loading] = useDownloadURL(storageRef.child(props.img.imagePath));	
+	// const [downloadUrl, loading] = useDownloadURL(storageRef.child(props.img.imagePath));	
   	return (<>
     	<div id={props.img.id} className="gridImg">
-    		{ loading && <CenteredPreloader  title={"Loading image"}/> }
-    		{downloadUrl && !loading &&
+    		{/* { loading && <CenteredPreloader  title={"Loading image"}/> } */}
+    		{/* {downloadUrl && !loading && */}
 			<MediaBox
   				options={{
   				  	inDuration: 200,
@@ -62,11 +62,11 @@ function LoadAndShowImage(props){
 				>
 			  	<img
 			    	alt={props.img.caption}
-			    	src={downloadUrl}
+			    	src={props.img.thumbURL}
 			    	className=" materialboxed galleryImg"
 			  	/>
-			</MediaBox>}
-			  	{downloadUrl && !loading && props.deleting === true && 
+			</MediaBox>
+			  	{props.deleting === true && 
 			  		<Button
     				className="red right"
     				node="button"
@@ -75,7 +75,7 @@ function LoadAndShowImage(props){
     				waves="light"
     				floating
   					icon={<Icon>delete</Icon>}
-  					onClick={()=>props.deleteCallback(props.img, props.index)}
+  					onClick={()=>props.deleteCallback(props.img)}
     			/>}
     	</div>
     </>);
@@ -88,7 +88,7 @@ function ShowColumn(props){
   
 		{props.index && 
 		 props.index.map(i => (i < props.images.length)? <LoadAndShowImage
-			 					key={props.images[i].id}
+			 					key={"img"+props.images[i].id}
 			 					img={props.images[i]}
 			 					deleting={props.deleting}
 			 					deleteCallback={props.deleteCallback}
@@ -128,78 +128,154 @@ export default  function Gallery(props) {
 
 }
 
+
+function createMediaBox(imgId, src, thumbSrc){
+	console.log("createMediaBox ", imgId);
+	
+	var elem = document.getElementById(imgId);
+	if(elem){
+		elem.src = src;
+    	var box = window.M.Materialbox.init(elem, {onCloseStart: ()=> elem.src = thumbSrc});
+    	box.open();
+	}else{
+		console.log("createMediaBox failed: imageId " + imgId + " does not exist");
+	}
+
+
+}
+
+
+//img
+//deleting
+//deleteCallback
+//user
+function ShowImg(props){
+	// const [downloadUrl, loading, error] = useDownloadURL(storageRef.child(props.img.imagePath));
+
+	// console.log(props.img.thumbURL);
+	// if(!props.img.thumbURL)return "";
+	
+
+	// const [showImageBox, setShowImageBox] = useState(false);
+
+// 
+// 	if(!props.img.thumbURL && !props.img.downloadURL){
+// 		console.log(props.img);
+// 		return "";
+// 	}
+
+
+	let thisId = (props.showUserGallery===true?"user_":"")+props.img.id;
+
+
+	return (<>
+		<li>
+			<img 
+				id={thisId}
+				alt={props.img.caption}
+				src={ props.img.thumbURL}
+				data-caption={props.img.caption}
+				className=" materialboxed galleryImg"
+				onClick={(e)=>
+							{
+								if(e.target.id === thisId){
+									createMediaBox(thisId, props.img.downloadURL, props.img.thumbURL);
+								}
+							}
+						}
+			/>
+				
+			{ props.deleting === true && 
+				<Button
+    				className="red right removeImageButton"
+    				node="button"
+    				small
+    				tooltip="Delete this image"
+    				waves="light"
+    				floating
+  					icon={<Icon>delete</Icon>}
+  					onClick={()=>props.deleteCallback(props.img)}
+    		/>}
+		</li>
+	</>)    	
+}
+
 function RenderGallery(props){
 
-	let query = db.collection("images").where("workshopId", "==", props.user.currentWorkshop);
+	let query = null;//db.collection("images").where("workshopId", "==", props.user.currentWorkshop);
 
 	if(props.showUserGallery === true){
-		query = query.where("uid", "==", props.user.id);
+		query = db.collection("images").where("workshopId", "==", props.user.currentWorkshop).where("uid", "==", props.user.id);
+	}else{
+		query = db.collection("images").where("workshopId", "==", props.user.currentWorkshop);
 	}
 	
 
 	const [images, imagesLoading] = useCollectionData(query); 
 	
 	let [deleting, setDeleting] = useState(false);
-	let [indices, setIndices] = useState([]);
+	// let [indices, setIndices] = useState([]);
 	// let nextPageToken = useRef(null);
 	// let currentPageToken = useRef(null);
 	// let originalBg  = useRef(null);
-	let [pctWidth, setPctWidth] = useState(20);
+	// let [pctWidth, setPctWidth] = useState(20);
 	let needsCalculateSize = useRef(true);
 	
 
-	if(needsCalculateSize.current === true && images && !imagesLoading){
-		calculateSize();
-	}
+	// if(needsCalculateSize.current === true && images && !imagesLoading){
+	// 	calculateSize();
+	// }
 
 
 
-	function calculateSize(){
-		let imgWidth = 200;
-		let galleryPadding=60;
-		let numColumns = Math.floor((getWidth()-galleryPadding)/imgWidth);
-		let imgsPerCol =2;
-		let modulo = 0;
-		if(images && images.length > 0){
-			imgsPerCol = Math.floor(images.length/numColumns);
-			modulo = images.length % numColumns;
-		}
+// 	function calculateSize(){
+// 		let imgWidth = 200;
+// 		let galleryPadding=60;
+// 		let numColumns = Math.floor((getWidth()-galleryPadding)/imgWidth);
+// 		let imgsPerCol =2;
+// 		let modulo = 0;
+// 		if(images && images.length > 0){
+// 			imgsPerCol = Math.floor(images.length/numColumns);
+// 			modulo = images.length % numColumns;
+// 		}
+// 
+// 		let newIndices = [];
+// 
+// 		let startAt = 0;
+// 		for(let x = 0; x < numColumns; x++){
+// 			
+// 			let num = imgsPerCol + ((x < modulo)? 1 : 0);
+// 			let index = [];
+// 			for(let i = startAt; i < startAt + num; i++){
+// 				index.push(i);
+// 			}
+// 			newIndices.push(index);
+// 
+// 			startAt += num;
+// 		}
+// 
+// 		// console.log("newIndices", newIndices);
+// 		setIndices(newIndices)
+// 
+// 		setPctWidth(100.0 / numColumns);
+// 
+// 		needsCalculateSize.current = false;
+// 		// console.log("calculateSize. imgs: ", imagesRefs, "  indices: ", indices);
+// 
+// 	}
 
-		let newIndices = [];
 
-		let startAt = 0;
-		for(let x = 0; x < numColumns; x++){
-			
-			let num = imgsPerCol + ((x < modulo)? 1 : 0);
-			let index = [];
-			for(let i = startAt; i < startAt + num; i++){
-				index.push(i);
-			}
-			newIndices.push(index);
+// 
+// 	useEffect(()=>{
+// 		window.addEventListener("resize", calculateSize);
+// 		return (()=>{
+// 			window.removeEventListener("resize", calculateSize);
+// 		})
+// 
+// 	});
 
-			startAt += num;
-		}
-
-		setIndices(newIndices)
-		setPctWidth(100.0 / numColumns);
-
-		needsCalculateSize.current = false;
-		// console.log("calculateSize. imgs: ", imagesRefs, "  indices: ", indices);
-
-	}
-
-
-
-	useEffect(()=>{
-		window.addEventListener("resize", calculateSize);
-		return (()=>{
-			window.removeEventListener("resize", calculateSize);
-		})
-
-	});
-
-	function deleteCallback(img, index){
-		needsCalculateSize.current = true;
+	function deleteCallback(img){
+		// needsCalculateSize.current = true;
 		deleteImg(img);
 
 		// let tempImgs = imagesRefs;
@@ -237,21 +313,61 @@ function RenderGallery(props){
 			</div>
 			<div id= {idPrefix+"GalleryImgs"} className="galleryImgs">
 				{ imagesLoading && <CenteredPreloader  title={"Loading gallery"}/> }
-				{(!imagesLoading && images && images.length >0 && indices.length > 0) ?
-
-				indices.map(index => <ShowColumn  
-					pctWidth={pctWidth} 
-					key={index} 
-					index={index} 
-					images={images} 
+				{(!imagesLoading && images && images.length >0) ?
+				<ul>
+				{images.map(img => <ShowImg
+					key={img.id} 
+					img={img} 
 					deleting={deleting}
 				 	deleteCallback={deleteCallback}
 				 	user={props.user}
-				 	/>)
+				 	showUserGallery={props.showUserGallery}
+				 	/>)}
+				<li></li>
+				</ul>
 				:
 				<p> Empty gallery! </p>}
 			</div>
-
 		</div>
 	</>
 }
+
+// function GetOtherImages(props){
+// 	async function getImgs(){
+// 		try{
+// 			
+// 			let all = await storageRef.child("images").listAll();
+// 				// let allItems = [];
+// 				for(let i = 0; i < all.prefixes.length; i++){
+// 					// console.log(all.prefixes[i].name);
+// 					let uid = all.prefixes[i].name;
+// 					let folder = await all.prefixes[i].listAll();
+// 					if(folder.items.length > 0 ){
+// 						// console.log(folder.items);
+// 						for(let i = 0; i < folder.items.length; i++){
+// 							let url = await folder.items[i].getDownloadURL();
+// 							// console.log(url);
+//                 			let newImage = ImageData(uid, "XIWfFl9mm0GxYZb7svG6", url, "", folder.items[i].fullPath);
+//         					
+//                 			addDataToDb("images" ,newImage, true, "id");
+// 							// console.log(newImage);
+// 						}
+// 						// console.log(folder.items);
+// 						// allItems = allItems.concat(folder.items);
+// 						// console.log(allItems);
+//     	 					// allItems.push(...folder.items);
+// 					}
+// 		    	}
+// 		}catch(error) {
+//   			console.log("Loading gallery failed with error:", error);
+// 		}
+// 	}
+// 	return <Button
+//     		className="red white-text"
+//     		node="button"
+//     		waves="light"
+//   			onClick={()=>getImgs()}
+// 		>
+// 		Get All Images
+// 		</Button>
+// }
