212fcd7a8a4bd72e154fbc0bb358429c57f10c18
Roy Macdonald <macdonald.roy@gmail.com>
2021-05-20 16:14:44 -0700

    added credentials

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
 const config = {
-  apiKey: "***REMOVED***",
+apiKey: "***REMOVED***",
   authDomain: "space-messengers.firebaseapp.com",
   projectId: "space-messengers",
   storageBucket: "space-messengers.appspot.com",
