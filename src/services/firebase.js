88b215fe12ff0818a94c1bcedba24031c8dacc75
Roy Macdonald <macdonald.roy@gmail.com>
2021-05-21 11:28:46 -0700

    Updated keys

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,16 @@
 import firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
-const config = {
-apiKey: "***REMOVED***",
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
   authDomain: "space-messengers.firebaseapp.com",
   projectId: "space-messengers",
   storageBucket: "space-messengers.appspot.com",
   messagingSenderId: "***REMOVED***",
-  appId: "1:***REMOVED***:web:***REMOVED***"
+  appId: "1:***REMOVED***:web:42b1ce016c93bbfe5f969d"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth;
 export const db = firebase.firestore();
