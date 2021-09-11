d12b410e9725ee1cf312197f601a8c531b46a22c
Roy Macdonald <macdonald.roy@gmail.com>
2021-09-11 17:19:27 -0500

    Updates to private_api

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,20 @@ import firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCmMvwVbcCjRckVFKsFhdE-jVUVUP_1Zao",
+  apiKey: "***REMOVED***",
   authDomain: "space-messengers.firebaseapp.com",
   projectId: "space-messengers",
   storageBucket: "space-messengers.appspot.com",
   messagingSenderId: "***REMOVED***",
-  appId: "1:***REMOVED***:web:315bc715749b60915f969d"
+  appId: "1:***REMOVED***:web:53b5214e4cfbd0cd5f969d"
 };
+
+
+
+
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth;
