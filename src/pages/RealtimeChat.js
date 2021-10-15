9d5774568e84cc9175cd123ffa4214374a5de076
Roy Macdonald <macdonald.roy@gmail.com>
2021-10-15 01:29:49 -0500

    Added admin stuff for realtimechat

--- src/pages/RealtimeChat.js
+++ src/pages/RealtimeChat.js
@@ -1,13 +1,12 @@
 import React, { useRef, useState,useEffect } from "react";
-
-import { db } from "../services/firebase";
+import { db, auth } from "../services/firebase";
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 
 import { useCollectionData, useDocumentData } from 'react-firebase-hooks/firestore';
 
-import { Icon } from 'react-materialize';
+import { Icon, Button, Badge } from 'react-materialize';
 
 import '../css/realtimechat.css';
 
@@ -16,7 +15,7 @@ import { randomColorHSL } from "../helpers/Types.js"
 function getMyMessages(){
     let myMessages = sessionStorage.getItem("myMessages")
     if(myMessages){
-        console.log("myMessages string", myMessages);
+        // console.log("myMessages string", myMessages);
         return JSON.parse(myMessages);
     }else{
         return [];
@@ -62,14 +61,38 @@ function removeFromMyMessages(toRemove){
 
 
 export default function RealtimeChat(props) {
+    // console.log("auth().currentUser ", auth().currentUser);
+
+    
+    
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isShowingAll, setIsShowingAll] = useState(false);
+    useEffect (() => {
+    if(auth().currentUser && !auth().currentUser.isAnonymous){
+        db.collection('users').doc(auth().currentUser.uid).get().then(user=>{
+            if (user.exists) {
+                // console.log("user.data().type", user.data().type);
+                if(user.data().type === "admin"){
+                    setIsAdmin(true);
+                    // console.log("setIsAdmin(true);");
+                }
+            }
+        }).catch((error) => {
+            console.log("Error getting currentUser:", error);
+        });
+    }
+     },[]);
+
     const dummy = useRef();
 
     const color = useRef(randomColorHSL());
 
-    let query = db.collection("realtime")
-    .where('wasShown', '==', false)
-    .where('isShowing', '==', true)
-    .orderBy('timestamp');//.limit(15);
+    let query = db.collection("realtime");
+    if(!isShowingAll){
+        query = query.where('wasShown', '==', false)
+        .where('isShowing', '==', true);
+    }
+    query = query.orderBy('timestamp');//.limit(15);
    
 
     let waitingQuery = db.collection("realtime")
@@ -94,9 +117,18 @@ export default function RealtimeChat(props) {
     // });
 
 
+
+
     useEffect (() => {
         dummy.current.scrollIntoView({ behavior: 'smooth' });
     },[messages],);
+
+    const showAllMessagesCallback = ()=>{
+        if(isAdmin === true){
+            setIsShowingAll(!isShowingAll);
+        }
+    }
+
 
     const sendMessage = async (e) => {
         
@@ -145,15 +177,17 @@ export default function RealtimeChat(props) {
         }
     }
 
+
     return (<>
         <div className="realtimeContainer">
+            {isAdmin && <RenderAdminBar showAllMessagesCallback={showAllMessagesCallback} isShowingAll={isShowingAll}/>}
             <div className="realtimeMessagesContainer">
                 {loadingMessages && <div>loading...</div>}
                 <ul>
                     {/* {!loadingMessages && messages && messages.slice(0).reverse().map(msg =>  */}
                     {/*     <RenderMessage key={msg.id} message={msg} />)} */}
                     {!loadingMessages && messages && messages.map(msg => 
-                        <RenderMessage key={msg.id} message={msg} myMessages={myMessages} />)}
+                        <RenderMessage key={msg.id} message={msg} myMessages={myMessages} isAdmin={isAdmin} isShowingAll={isShowingAll} />)}
                 </ul>
                 <span ref={dummy}></span>
             </div>
@@ -208,6 +242,33 @@ return (<>
 }
 
 
+function deleteCallback(id){
+    // console.log("deleteCallback ", id);
+
+db.collection("realtime").doc(id).delete().then(() => {
+    console.log("Message successfully deleted!");
+}).catch((error) => {
+    console.error("Error removing message: ", error);
+});
+
+
+}
+
+function RenderAdminBar(props){
+    return (<>
+        <div className="realtimeAdminTopBar">
+            <Button
+                    className="teal right adminShowAllMessagesButton"
+                    node="button"
+                    small
+                    tooltip="Show all messages"
+                    waves="light"
+                    onClick={()=>props.showAllMessagesCallback()}
+            >{props.isShowingAll?"Only show realtime messages":"Show all Messages"}</Button>
+        </div>
+    </>);
+}
+
 
 function RenderMessage( props){
     let style = {};
@@ -216,17 +277,38 @@ function RenderMessage( props){
      messageClass += "ownChatMessage";
     }
     // (props.message.uid === auth().currentUser.uid)?"ownChatMessage":"";
+    // console.log("props.isAdmin", props.isAdmin);
+
+    let badgeClass = "white-text realtimeMessageBadge " + (props.message.wasShown?"blue":"yellow");
 
 return (<>
 
     <li id={"realtimemessage-"+props.message.id}
                 className= {messageClass}
                 style={style}
-            >
-            
+            >  
             <div className="realtimeCard-header valign-wrapper" >
+
+            { props.isAdmin && 
+                <Button
+                    className="red right removeImageButton"
+                    node="button"
+                    small
+                    tooltip="Delete this message"
+                    waves="light"
+                    floating
+                    icon={<Icon>delete</Icon>}
+                    onClick={()=>deleteCallback(props.message.id)}
+            />}
+            { props.isAdmin && props.isShowingAll &&
+                <div>
+                    <Badge className={badgeClass}>{"was shown " + (props.message.wasShown?"YES": "NO")  } </Badge>
+                    {props.message.isShowing && <Badge className="green white-text realtimeMessageBadge">{"is showing now"} </Badge>}
+                    
+                </div>
+            }
                 {/* <img src={props.user?props.user.photoURL:""} alt="" className="circle messageHeaderImg "/>  */}
-                {props.message.ProfileName}
+                {props.message.ProfileName} 
             </div>
             <div className="realtimeCard-content white-text">
                 {props.message.Body}
